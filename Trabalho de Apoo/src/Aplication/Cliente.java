323dd00dd5d8827287fb8d30e3ccdc45f9c5e77e
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2025-05-27 08:32:01 -0300

    trabalho para ser produzido em casa pois é nota

---
+++ Trabalho de Apoo/src/Aplication/Cliente.java
@@ -0,0 +1,37 @@
+package Aplication;
+
+public class Cliente {
+    private String nome;
+    private String CPF;
+    private String email;
+
+    public Cliente(String nome, String CPF, String email) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
