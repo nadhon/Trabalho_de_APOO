f739aeac59b792958ef32db1353136c50da319f6
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2025-06-11 10:18:48 -0300

    Update Tema.java

--- Trabalho de Apoo/src/AppDeskTop/Tema.java
+++ Trabalho de Apoo/src/AppDeskTop/Tema.java
@@ -5,6 +5,9 @@ public class Tema {
     private String corDaFonte;
     private String nomeDaFonte;
     private int tamanhoDaFonte;
+
+    private Tema(String corDeFundo, String corDaFonte, int tamanhoDaFonte){
+    }
     public String getCorDeFundo() {
         return corDeFundo;
     }
@@ -21,13 +24,28 @@ public class Tema {
         return tamanhoDaFonte;
     }
 
+    public Tema(String corDeFundo, String corDaFonte, String nomeDaFonte, int tamanhoDaFonte) {
+        this.corDeFundo = corDeFundo;
+        this.corDaFonte = corDaFonte;
+        this.nomeDaFonte = nomeDaFonte;
+        this.tamanhoDaFonte = tamanhoDaFonte;
+    }
+
     public static class  TemaBuilder{
         private String corDeFundo;
         private String corDaFonte;
         private String nomeDaFonte;
-
-        public class comCorDeFundo(String corDeFundo){
-            this.comCorDeFundo= corDeFundo;
+        public String comCorDeFundo(String corDeFundo){
+            this.corDeFundo = corDeFundo;
+            return corDeFundo;
+        }
+        public String comCorDaFonte(String corDeFundo){
+            this.corDaFonte = corDaFonte;
+            return corDaFonte;
+        }
+        public String comNomeDaFonte(String corDeFundo){
+            this.nomeDaFonte = nomeDaFonte;
+            return nomeDaFonte;
         }
     }
     public void setCorDeFundo(String corDeFundo) {
@@ -45,6 +63,7 @@ public class Tema {
     public void setTamanhoDaFonte(int tamanhoDaFonte) {
         this.tamanhoDaFonte = tamanhoDaFonte;
     }
+    @Override
     public String toString() {
         return "Tema{" +
                 "corDeFundo='" + corDeFundo + '\'' +
@@ -53,7 +72,7 @@ public class Tema {
                 ", tamanhoDaFonte=" + tamanhoDaFonte +
                 '}';
     }
-
-
-
+    public Tema build(){
+        return new Tema(corDeFundo, corDaFonte, tamanhoDaFonte);
+    }
 }
